cacde2ab3004bb250672b3ff34beb9cfd20b1b92
Priyanshi Saxena <107347596+priyanshii026@users.noreply.github.com>
2023-07-02 11:51:47 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+from art import logo
+def deal_card():
+    cv = {'A': 11, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 'J': 10, 'Q': 10, 'K': 10}
+    card = random.choice(list(cv.keys()))
+    return cv[card]
+
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+        
+    return sum(cards)
